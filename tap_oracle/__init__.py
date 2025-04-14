e6bddc8278d60817a6a16f8c425602b78dda4f49
jnorris3 <jnorris3@buffalo.edu>
2025-04-14 14:08:31 -0400

    test

--- tap_oracle/__init__.py
+++ tap_oracle/__init__.py
@@ -680,7 +680,9 @@ def main_impl():
 
 def main():
     try:
+        LOGGER.info("Test")
         main_impl()
+        LOGGER.info("Test")
     except Exception as exc:
         LOGGER.critical(exc)
         raise exc
