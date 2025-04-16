43466007dce1794d849ae5b1d2f4e0417a3b3a11
jnorris3 <jnorris3@buffalo.edu>
2025-04-16 12:30:07 -0400

    test

--- tap_oracle/sync_strategies/full_table.py
+++ tap_oracle/sync_strategies/full_table.py
@@ -74,7 +74,7 @@ def sync_view(conn_config, stream, state, desired_columns):
                                                        desired_columns,
                                                        time_extracted)
          singer.write_message(record_message)
-         singer.write_message(counter.metric())
+         singer.write_message(singer.StateMessage(value=(counter.metric())))
          counter.increment()
 
    #always send the activate version whether first run or subsequent
